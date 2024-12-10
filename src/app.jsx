5deec212fa52909ec42356ed7f870e8d7f220724
Christian Frenzl <chriseff@users.noreply.github.com>
2024-12-10 22:21:45 +0100

    chore: set up prettier + format code

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+const root = createRoot(document.body)
+root.render(<h2>Hello from React!</h2>)
