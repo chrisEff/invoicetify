b9be06d712c8a02a17141e63606351abe4339613
Christian Frenzl <chriseff@users.noreply.github.com>
2025-10-07 20:19:30 +0200

    build: update dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,6 @@
 module.exports = {
-	filterResults: (name, semver) => {
-		// 1.7.4 has an incompatibility with electron forge
-		return !(name === '@vercel/webpack-asset-relocator-loader' && semver.upgradedVersion === '1.7.4')
+	filterResults: name => {
+		// 1.7.4 (+later) has an incompatibility with electron forge
+		return !(name === '@vercel/webpack-asset-relocator-loader')
 	},
 }
