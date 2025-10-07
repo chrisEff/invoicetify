b6453acf183a7137e3d12ff6ddb8d70b77de1169
Christian Frenzl <chriseff@users.noreply.github.com>
2025-10-07 19:04:12 +0200

    feat: better demo data

--- src/demoData/items.ts
+++ src/demoData/items.ts
@@ -2,25 +2,25 @@ import { getRandomNumber } from '../util/getRandomNumber'
 
 const items = [
 	{
-		title: 'T-Shirt, size: M, color: blue',
+		title: 'T-Shirt, size M, color blue',
 		quantity: 1,
 		unitPrice: 19.9,
 		vat: 19,
 	},
 	{
-		title: 'Sweater, size: M, color: orange',
+		title: 'Sweater, size M, color orange',
 		quantity: 1,
 		unitPrice: 49.9,
 		vat: 19,
 	},
 	{
-		title: 'Jeans, size: 32, color: black',
+		title: 'Jeans, size 32/30, color black',
 		quantity: 1,
 		unitPrice: 99.9,
 		vat: 19,
 	},
 	{
-		title: 'Socks, size: 43-46, color: white',
+		title: 'Socks, size 43-46, color white',
 		quantity: 1,
 		unitPrice: 5.99,
 		vat: 19,
