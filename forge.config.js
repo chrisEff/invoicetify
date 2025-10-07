0db4fe624038302ccc3ca72ef44cef73dad1d60e
Christian Frenzl <chriseff@users.noreply.github.com>
2025-10-07 19:04:31 +0200

    build: change webpack port to `3042` in order to not collide with default `3000` from other React projects

--- forge.config.js
+++ forge.config.js
@@ -33,6 +33,7 @@ module.exports = {
 			name: '@electron-forge/plugin-webpack',
 			config: {
 				mainConfig: './webpack.main.config.js',
+				port: 3042,
 				renderer: {
 					config: './webpack.renderer.config.js',
 					entryPoints: [
